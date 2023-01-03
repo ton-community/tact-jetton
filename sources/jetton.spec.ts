b84954d05119bd2faae6b1efd75c586d39f78612
AlexG <39581753+Reveloper@users.noreply.github.com>
2023-01-03 17:12:27 +0400

    Initial commit

---
+++ sources/jetton.spec.ts
@@ -0,0 +1,26 @@
+import { toNano } from "ton-core";
+import { ContractSystem } from "ton-emulator";
+import {SampleJetton, SampleJetton_init} from './output/jetton_SampleJetton';
+
+describe('jetton', () => {
+    it('should deploy', async () => {
+
+        // Create jetton
+        let system = await ContractSystem.create();
+        let owner = system.treasure('owner');
+        let contract = system.open(await SampleJetton.fromInit(owner.address, null));
+        let tracker = system.track(contract.address);
+
+        // Mint
+        await contract.send(owner, { value: toNano(1) }, { $$type: 'Mint', amount: toNano(1000000) });
+        await system.run();
+        expect(tracker.events()).toMatchSnapshot();
+
+        // Check owner
+        expect((await contract.getOwner()).toString()).toEqual(owner.address.toString());
+
+        // Data
+        let data = await contract.getGetJettonData();
+        // console.warn(data);
+    });
+});
